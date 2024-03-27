2078caa67f940240911c0cf3ec6620905e0982d1
og3da <rapha488@gmail.com>
2024-03-27 17:08:05 -0300

    criando interface Champions, implementação da interface e buscando todos campeões no banco de dados H2

---
+++ src/main/java/me/dio/sdw24/domain/adapters/out/ChampionsJdbcRepository.java
@@ -0,0 +1,39 @@
+package me.dio.sdw24.domain.adapters.out;
+
+import me.dio.sdw24.domain.model.Champions;
+import me.dio.sdw24.domain.ports.ChampionsRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ChampionsJdbcRepository implements ChampionsRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Champions> championsRowMapper;
+
+    public ChampionsJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.championsRowMapper = (rs, rowNum) -> new Champions(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("role"),
+                rs.getString("lore"),
+                rs.getString("image_url")
+        );
+    }
+
+    @Override
+    public List<Champions> findAll() {
+        return jdbcTemplate.query("SELECT * FROM CHAMPIONS", championsRowMapper);
+    }
+
+    @Override
+    public Optional<Champions> findById(Long id) {
+        Champions champion = jdbcTemplate.queryForObject("SELECT * FROM CHAMPIONS WHERE id = {}", championsRowMapper, id);
+        return Optional.ofNullable(champion);
+    }
+}
