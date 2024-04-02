2c9390fcc6d37f5aa00af1b1a76d5542eff76ab2
og3da <rapha488@gmail.com>
2024-04-01 22:18:08 -0300

    impl. logs (pergunta enviada)

--- src/main/java/me/dio/sdw24/adapters/in/AskChampionRestController.java
+++ src/main/java/me/dio/sdw24/adapters/in/AskChampionRestController.java
@@ -1,7 +1,10 @@
 package me.dio.sdw24.adapters.in;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import me.dio.sdw24.adapters.in.exception.GlobalExceptionHandler;
 import me.dio.sdw24.application.AskChampionUseCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Campeões (champions)", description = "Endpoints do domínio de campeões do LOL.")
@@ -9,10 +12,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/champions")
 public record AskChampionRestController(AskChampionUseCase useCase) {
 
+    static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @CrossOrigin
     @PostMapping("/{championId}/ask")
     public AskChampionResponse askChampion(@PathVariable Long championId, @RequestBody AskChampionRequest request) {
 
+        logger.info("=== QUESTION RECIEVED: %s ===".formatted(request.question()));
         String answer = useCase().askChampion(championId, request.question());
 
         return new AskChampionResponse(answer);
